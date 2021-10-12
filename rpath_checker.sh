80bdc6afe38fa594a4bb5115f033eb07299dee3c
ms101 <ms101@users.noreply.github.com>
2021-10-12 10:57:40 +0200

    extended

--- rpath_checker.sh
+++ rpath_checker.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 # check for occurences of ".." or "." in RPATH and RUNPATH of all binaries executable (by user)
 
-find / -type f -and -executable -not -path "/proc" 2>/dev/null | xargs -I% readelf -W -d "%" 2>/dev/null | grep path | grep "\."
+#find / -type f -and -executable ! -path "/proc" 2>/dev/null | xargs -I% readelf -W -d "%" 2>/dev/null | grep path | grep "\."
+find / -type f -and -executable ! -path "/proc/*" ! -path "/.xy/*" 2>/dev/null | xargs -I% sh -c 'readelf -W -d "%" 2>/dev/null | grep path | grep -E "\.:|:\.|\.\." && echo "%"' | tee rpath_complete
 # s/-executable/-readable/
